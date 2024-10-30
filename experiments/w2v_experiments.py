c0eef5b35c9922dcf37d502618398829cddc14cc
Ronald van den Broek <ronald.vandenbroek@live.nl>
2024-10-30 20:27:52 +0100

    Renaming Experiment Files

--- experiments/w2v_experiments.py
+++ experiments/w2v_experiments.py
@@ -75,7 +75,7 @@ def W2V_finetuned():
     w2v_vector_sizes = [20]
     w2v_window_sizes = [2]
     pre_train_percentage = [0]
-    batch_sizes = [2]
+    batch_sizes = [8] #RCVDB: TODO finetune batch size and buckets
     buckets = [[3,4,5,6,7,8,9]]
 
     ads_w2v_embedding_search = []
@@ -88,6 +88,41 @@ def W2V_finetuned():
             prefix=True, 
             bucket_boundaries=bucket,
             categorical_encoding=EncodingCategorical.WORD_2_VEC_ATC,
+            numerical_encoding=EncodingNumerical.MIN_MAX_SCALING,
+            pretrain_percentage=pre_train_percentage,
+            w2v_vector_size=w2v_vector_size,
+            w2v_window_size=w2v_window_size)))
+        
+    # ads_w2v_embedding_search.append(dict(ad=DAE, fit_kwargs=dict( 
+    #         batch_size=8, 
+    #         prefix=True, 
+    #         bucket_boundaries = [3,4,5,6,7,8,9],
+    #         categorical_encoding=EncodingCategorical.ONE_HOT,
+    #         numerical_encoding=EncodingNumerical.MIN_MAX_SCALING,
+    #         pretrain_percentage=0,
+    #         w2v_vector_size=0,
+    #         w2v_window_size=0)))
+    
+    return ads_w2v_embedding_search,run_name
+
+def W2V_no_averaging():
+    run_name = 'W2V_no_averaging'
+    w2v_vector_sizes = [20]
+    w2v_window_sizes = [2]
+    pre_train_percentage = [0]
+    batch_sizes = [8] #RCVDB: TODO finetune batch size and buckets
+    buckets = [[3,4,5,6,7,8,9]]
+
+    ads_w2v_embedding_search = []
+    w2v_combinations = list(itertools.product(w2v_vector_sizes, w2v_window_sizes, pre_train_percentage, batch_sizes, buckets))
+    for w2v_combination in w2v_combinations:
+        w2v_vector_size, w2v_window_size, pre_train_percentage, batch_size, bucket = w2v_combination
+
+        ads_w2v_embedding_search.append(dict(ad=DAE, fit_kwargs=dict( 
+            batch_size=batch_size, 
+            prefix=True, 
+            bucket_boundaries=bucket,
+            categorical_encoding=EncodingCategorical.WORD_2_VEC_C,
             numerical_encoding=EncodingNumerical.MIN_MAX_SCALING,
             pretrain_percentage=pre_train_percentage,
             w2v_vector_size=w2v_vector_size,
