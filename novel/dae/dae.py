Implementing universal masking + fixed vector bugfix

novel/dae/dae.py
@@ -22,7 +22,7 @@ from baseline.binet.core import NNAnomalyDetector
 from utils import process_results
 from utils.dataset import Dataset
 from utils.enums import AttributeType, EncodingCategorical, Heuristic, Strategy, Mode, Base
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 from utils.settings.settings_multi_task import SettingsMultiTask
 
@@ -214,7 +214,7 @@ class DAE(NNAnomalyDetector):
             # metrics=['accuracy']
         )
 
-        model.summary()
+        # model.summary()
 
         return model
 
@@ -272,7 +272,8 @@ class DAE(NNAnomalyDetector):
             sorted_perspectives  = np.concatenate((perspective_array[categorical_mask], perspective_array[~categorical_mask]))
 
             anomaly_perspectives = sorted_perspectives
-
+            # self._attribute_dims = np.array([self.vector_size] * len(self.attribute_dims))
+        
         bucket_trace_level_abnormal_scores = []
         bucket_event_level_abnormal_scores = [] 
         bucket_attr_level_abnormal_scores = []

utils/dataset.py
@@ -370,7 +370,7 @@ class Dataset(object):
                 dictionary_starting_index += dictionary_size
 
                 # If categorical encoding is none then categorical values are treated as numerical thus can scale
-                if self.categorical_encoding == EncodingCategorical.NONE or self.categorical_encoding == EncodingCategorical.FIXED_VECTOR:
+                if self.categorical_encoding == EncodingCategorical.NONE:# or self.categorical_encoding == EncodingCategorical.FIXED_VECTOR:
                     if self.numerical_encoding == EncodingNumerical.MIN_MAX_SCALING:
                         feature_columns[key] = self._min_max_scaling(feature_columns[key])
                     replace_attribute_type = AttributeType.NUMERICAL
@@ -564,20 +564,22 @@ class Dataset(object):
 
     def flat_w2v_features_2d_average(self, trace2vec=False):
         w2v_encoder = self._build_w2v_model()
-        # self._attribute_dims = np.array([self.vector_size] * len(self.attribute_dims))
-        return w2v_encoder.encode_flat_features_2d_average(trace2vec=trace2vec)
+        features, self._attribute_dims = w2v_encoder.encode_flat_features_2d_average(trace2vec=trace2vec)
+        return features
         
     def flat_w2v_features_2d(self, trace2vec=False):
         w2v_encoder = self._build_w2v_model()
-        self._attribute_dims = np.array([self.vector_size] * len(self.attribute_dims))
-        return w2v_encoder.encode_flat_features_2d(attribute_keys=self.event_log.event_attribute_keys, trace2vec=trace2vec)
+        features, self._attribute_dims = w2v_encoder.encode_flat_features_2d(attribute_keys=self.event_log.event_attribute_keys, trace2vec=trace2vec)
+        return features
     
     def flat_fixed_vector_features_2d(self):
         fixed_vector_encoder = FixedVectorEncoder(
             vector_size=self.vector_size, 
-            features=self.features)
-        self._attribute_dims = np.array([self.vector_size] * len(self.attribute_dims))
-        return fixed_vector_encoder.flat_features_2d()
+            features=self.features,
+            attribute_types=self.attribute_types,
+            event_attribute_keys=self.event_log.event_attribute_keys)
+        features, self._attribute_dims = fixed_vector_encoder.flat_features_2d()
+        return features
     
     def flat_elmo_features_2d(self):
         elmo_encoder = ProcessELMoEncoder(

utils/embedding/w2v.py
@@ -2,11 +2,12 @@ from gensim.models import Word2Vec
 import numpy as np
 
 from sklearn.decomposition import PCA
+from tqdm import tqdm
 
 from processmining.case import Case
 from processmining.log import EventLog
 from utils.embedding.attribute_dictionary import AttributeDictionary
-from utils.embedding.util import fourier_encoding
+from utils.embedding.util import fourier_encoding, recalculate_attribute_dimensions
 from utils.enums import AttributeType
 from utils.fs import FSSave
 
@@ -56,8 +57,8 @@ class ProcessWord2VecEncoder():
                 w2v_model:Word2Vec = self.create_model(pretrain_sentences)
                 self.w2v_models[attribute_key] = w2v_model
 
-                if fs_save is not None:
-                    self._save_embedding_space(w2v_model, attribute_key, pretrain_percentage)
+                # if fs_save is not None:
+                #     self._save_embedding_space(w2v_model, attribute_key, pretrain_percentage)
 
     def create_training_data(self, pretrain_cases, attribute_key, encoder:AttributeDictionary):
         pretrain_sentences = []
@@ -99,7 +100,7 @@ class ProcessWord2VecEncoder():
         w2v_feature_names = []
         numeric_features = []
         numeric_feature_names = []
-        for index, (feature, attribute_type, attribute_key) in enumerate(zip(self.features, self.attribute_types, self.event_attribute_keys)):
+        for index, (feature, attribute_type, attribute_key) in tqdm(enumerate(zip(self.features, self.attribute_types, self.event_attribute_keys)), "Encoding W2V Features"):
             # RCVDB: TODO Experimental, not encoding start and end event
             # feature_experimental = []
             # for case in feature:
@@ -117,10 +118,11 @@ class ProcessWord2VecEncoder():
                             else:
                                 trace_attributes.append(self.zero_vector)
                         encoded_feature.append(trace_attributes)     
-                    numeric_features.append(encoded_feature)
+                    numeric_features.append(np.array(encoded_feature, dtype=np.float32))
                 else:
                     numeric_features.append(np.array(feature,dtype=np.float32))
                 numeric_feature_names.append(attribute_key)
+                
             elif attribute_type == AttributeType.CATEGORICAL:
                 encoded_feature = []
                 for attr_trace in feature:
@@ -143,60 +145,56 @@ class ProcessWord2VecEncoder():
             
         return np.array(w2v_features, dtype=np.float32), np.array(numeric_features, dtype=np.float32), np.array(numeric_feature_names), np.array(w2v_feature_names)
 
-    def encode_flat_features_2d(self, attribute_keys, trace2vec=False):
-        w2v_features, numeric_features, numeric_feature_names, w2v_feature_names = self.encode_features(average=False, match_numerical=True)
+    def encode_flat_features_2d(self, attribute_keys, trace2vec=False, match_numerical=False):
+        # if numerical_encoding == Vector: w2v_features, numeric_features, numeric_feature_names, w2v_feature_names = self.encode_features(average=False, match_numerical=True)
+        w2v_features, numeric_features, numeric_feature_names, w2v_feature_names = self.encode_features(average=False, match_numerical=match_numerical)
         # print(w2v_features.shape, numeric_features.shape)
-        transposed_w2v_features = np.transpose(w2v_features, (1, 2, 0, 3))
-        transposed_numeric_features = np.transpose(numeric_features, (1, 2, 0, 3))
+
         # print(transposed_w2v_features.shape, transposed_numeric_features.shape)
-        print(numeric_feature_names, w2v_feature_names)
+        # print(numeric_features.shape, numeric_feature_names)
+        # print(w2v_features.shape, w2v_feature_names)
+        # print(attribute_keys)
 
-        # Shapes of the input data
-        num_traces, num_events, num_numeric_features, vector_size = transposed_numeric_features.shape
-        _, _, num_w2v_features, _ = transposed_w2v_features.shape
+        transposed_w2v_features = np.transpose(w2v_features, (1, 2, 0, 3))
+        transposed_numeric_features = np.transpose(numeric_features, (1, 2, 0))
 
-        # Initialize the merged array
-        # RCVDB: Due to memory allocation with real world datasets use float32
-        merged_features = np.zeros((num_traces, num_events, num_numeric_features + num_w2v_features, vector_size), dtype=np.float32)
-        # print(merged_features.shape)
-              
-        # Keep track of the current indices for numeric and w2v features
-        numeric_index, w2v_index = 0, 0
-
-        # Iterate over dataset.attribute_keys to place each feature in the correct order
-        for i, key in enumerate(attribute_keys):
-            # print(key)
-            if key in numeric_feature_names:
-                # print("Numeric feature shape:", transposed_numeric_features[:, :, numeric_index, :].shape)
-                # print("Target shape:", merged_features[:, :, i, :].shape)
-                # print(numeric_index)
-                # Place numeric feature in the merged array
-                merged_features[:, :, i, :] = transposed_numeric_features[:, :, numeric_index, :]
-                numeric_index += 1
-            elif key in w2v_feature_names:
-                # print("W2V feature shape:", transposed_w2v_features[:, :, w2v_index, :].shape)
-                # print("Target shape:", merged_features[:, :, i, :].shape)
-                # print(w2v_index)
-                # Place w2v feature in the merged array
-                merged_features[:, :, i, :] = transposed_w2v_features[:, :, w2v_index, :]
-                w2v_index += 1
-            else:
-                raise ValueError(f"Unexpected attribute key '{key}' not found in either feature list.")
+        # print(transposed_numeric_features.shape, numeric_feature_names)
+        # print(transposed_w2v_features.shape, w2v_feature_names)
+        # print(attribute_keys)
         
-        # print(merged_features.shape)
-        dim0, dim1, dim2, dim3 = merged_features.shape
-        merged_features = np.reshape(merged_features, (dim0, dim1, dim2 * dim3))#, order='C')
-        # print(merged_features.shape)
-        merged_features = np.reshape(merged_features, (dim0, dim1 * dim2 * dim3))#, order='C')
-        # print(merged_features.shape)
+        reordered_slices = []
+        numeric_feature_names = list(numeric_feature_names)
+        w2v_feature_names = list(w2v_feature_names)
+        # Iterate through each feature in the total_order
+        for feature in attribute_keys:
+            if feature in numeric_feature_names:
+                feature_index = numeric_feature_names.index(feature)
+                if match_numerical:
+                    reordered_slices.append(transposed_numeric_features[:, :, feature_index:feature_index+1])  # Retain shape (35483, 13, 1)
+                else:
+                    reordered_slices.append(transposed_w2v_features[:, :, feature_index, :])  # Shape (35483, 13, 200)
+            elif feature in w2v_feature_names:
+                feature_index = w2v_feature_names.index(feature)
+                reordered_slices.append(transposed_w2v_features[:, :, feature_index, :])  # Shape (35483, 13, 200)
+
+        # Concatenate all slices along the last axis
+        merged_features = np.concatenate(reordered_slices, axis=-1)
+
+        # Generates a single encoding per case that can be prepended to the w2v encoding
+        trace_encoding = np.mean(merged_features, axis=(1))
+
+        print(merged_features.shape)
+        dim0, dim1, dim2 = merged_features.shape
+        flat_merged_features = np.reshape(merged_features, (dim0, dim1 * dim2))#, order='C')
+        print(flat_merged_features.shape)
 
         if trace2vec:
-            # Generates a single encoding per case that can be prepended to the w2v encoding
-            trace_encoding = np.mean(transposed_w2v_features, axis=(1, 2))
             # (num_cases, trace_encoding + num_attribute * num_events)
-            merged_features = np.concatenate((trace_encoding,merged_features), axis=1)  
+            flat_merged_features = np.concatenate((trace_encoding,flat_merged_features), axis=1)  
 
-        return merged_features
+        print(flat_merged_features.shape)
+
+        return flat_merged_features, recalculate_attribute_dimensions(self.attribute_types, self.vector_size, sort=False, match_numerical=match_numerical)
 
 
     def encode_flat_features_2d_average(self, trace2vec=False):
@@ -236,7 +234,7 @@ class ProcessWord2VecEncoder():
         assert not np.any(np.isnan(flat_w2v_numeric_features)), "Data contains NaNs!"
         assert not np.any(np.isinf(flat_w2v_numeric_features)), "Data contains Infs!"
 
-        return flat_w2v_numeric_features
+        return flat_w2v_numeric_features, recalculate_attribute_dimensions(self.attribute_types, self.vector_size, sort=True, match_numerical=False)
     
     def _save_embedding_space(self, model, attribute_key, pretrain_percentage):
         words = list(model.wv.index_to_key)
@@ -246,3 +244,4 @@ class ProcessWord2VecEncoder():
         word_vectors_2d = pca.fit_transform(word_vectors)
 
         self.fs_save.save_embedding_space(f"W2V_{attribute_key}", pretrain_percentage, words, word_vectors_2d)
+        

utils/process_results.py
@@ -25,18 +25,40 @@ def process_bucket_results(
     if categorical_encoding in (EncodingCategorical.TRACE_2_VEC_ATC, EncodingCategorical.TRACE_2_VEC_C):
         errors_unmasked = errors_unmasked[:, vector_size:]
 
-    # RCVDB: Mask empty events if no buckets are used or encoding method is not W2V
-    if not bucketing and categorical_encoding in (EncodingCategorical.WORD_2_VEC_ATC, EncodingCategorical.TRACE_2_VEC_ATC):
-    # Applies a mask to remove the events not present in the trace   
-    # (cases, flattened_errors) --> errors_unmasked
-    # (cases, num_events) --> dataset.mask (~ inverts mask)
-    # (cases, num_events, 1) --> expand dimension for broadcasting
-    # (cases, num_events, attributes_dim) --> expand 2nd axis to size of the attributes
-    # (cases, num_events * attributes_dim) = (cases, flattened_mask) --> reshape to match flattened error shape
-        errors = errors_unmasked * np.expand_dims(~dataset_mask, 2).repeat(attribute_dims.sum(), 2).reshape(
-            dataset_mask.shape[0], -1)
-    else:
+
+    # RCVDB: Generate the mask per bucket
+    if categorical_encoding in (EncodingCategorical.WORD_2_VEC_ATC, EncodingCategorical.TRACE_2_VEC_ATC):
+        attribute_type_counter = Counter(attribute_types)
+
+        dataset_mask = np.zeros(errors_unmasked.shape, dtype=bool)
+        for mask, case_length in zip(dataset_mask, case_lengths):
+            mask_length = attribute_type_counter[AttributeType.CATEGORICAL] * vector_size + attribute_type_counter[AttributeType.NUMERICAL] * case_length
+            mask[:mask_length] = True
+
+        errors = errors_unmasked * dataset_mask
+    elif categorical_encoding in (EncodingCategorical.ONE_HOT, EncodingCategorical.WORD_2_VEC_C, EncodingCategorical.TRACE_2_VEC_C, EncodingCategorical.FIXED_VECTOR):
+        dataset_mask = np.zeros(errors_unmasked.shape, dtype=bool)
+        for mask, case_length in zip(dataset_mask, case_lengths):
+            mask_length = int(attribute_dims.sum() * case_length)
+            mask[:mask_length] = True
+
+        errors = errors_unmasked * dataset_mask
+
+    else:    
         errors = errors_unmasked
+
+    # # RCVDB: Mask empty events if no buckets are used or encoding method is not W2V
+    # if not bucketing and categorical_encoding in (EncodingCategorical.WORD_2_VEC_ATC, EncodingCategorical.TRACE_2_VEC_ATC):
+    # # Applies a mask to remove the events not present in the trace   
+    # # (cases, flattened_errors) --> errors_unmasked
+    # # (cases, num_events) --> dataset.mask (~ inverts mask)
+    # # (cases, num_events, 1) --> expand dimension for broadcasting
+    # # (cases, num_events, attributes_dim) --> expand 2nd axis to size of the attributes
+    # # (cases, num_events * attributes_dim) = (cases, flattened_mask) --> reshape to match flattened error shape
+    #     errors = errors_unmasked * np.expand_dims(~dataset_mask, 2).repeat(attribute_dims.sum(), 2).reshape(
+    #         dataset_mask.shape[0], -1)
+    # else:
+    #     errors = errors_unmasked
 
     # If W2V all categorical events are embedded into a single vector
     if categorical_encoding in (EncodingCategorical.WORD_2_VEC_ATC, EncodingCategorical.TRACE_2_VEC_ATC):
@@ -61,7 +83,8 @@ def process_bucket_results(
     # np.sum for the total one-hot predictions being wrong
     # (attributes * events, cases)
     # RCVDB: TODO Sum the errors to amplify the difference between normal and anomalous data
-    errors_attr_split_summed = [np.sum(attribute, axis=1) for attribute in errors_attr_split]
+    # Does have the problem that longer traces will always have higher sums
+    errors_attr_split_summed = [np.mean(attribute, axis=1) for attribute in errors_attr_split]
 
     # if categorical_encoding == EncodingCategorical.WORD_2_VEC_ATC or categorical_encoding == EncodingCategorical.TRACE_2_VEC_ATC:
     #     errors_attr_split_summed = [np.sum(attribute, axis=1) for attribute in errors_attr_split]