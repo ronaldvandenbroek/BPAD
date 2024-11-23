44554b8960c272bb0c13d131a1ede1f4c09b1449
Ronald van den Broek <ronald.vandenbroek@live.nl>
2024-11-23 19:10:14 +0100

    Repository cleanup

--- verify_tensorflow.py
+++
@@ -1,6 +0,0 @@
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
-import tensorflow as tf
-tf.config.set_visible_devices([], 'GPU')
-print(tf.reduce_sum(tf.random.normal([1000, 1000])))
-# print(tf.config.list_physical_devices('GPU'))
