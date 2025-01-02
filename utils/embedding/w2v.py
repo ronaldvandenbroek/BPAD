0191a073ebfecf91da0f26883971d3c60bdcbeae
Ronald van den Broek <ronald.vandenbroek@live.nl>
2025-01-02 17:10:35 +0100

    Optimised w2v encoding

--- utils/embedding/w2v.py
+++ utils/embedding/w2v.py
@@ -21,11 +21,16 @@ class TransformerWord2VecEncoder(Layer):
         self.dim_model = dim_model
         self.sequence_length = sequence_length
 
+        self.num_attributes = len(attribute_keys)
+        case_length = sequence_length / self.num_attributes
+        self.case_length = tf.cast(case_length, tf.int32)
+        print("case_length", self.case_length)
+
         # Create a dictionary to map attribute keys to their index
         attribute_key_dict, attribute_keys_tensor = TransformerWord2VecEncoder._convert_encoder_keys(encoders)
 
-        self.attribute_key_trace_mask, self.categorical_mask, self.numerical_mask = TransformerWord2VecEncoder._create_attribute_masks(
-            attribute_keys, attribute_key_dict, sequence_length)
+        self.attribute_key_trace_mask, self.categorical_mask, self.numerical_mask, self.attribute_mask = TransformerWord2VecEncoder._create_attribute_masks(
+            attribute_keys, attribute_key_dict, self.case_length)
 
         extracted_w2v_models = TransformerWord2VecEncoder._create_w2v_models(encoders, dim_model)
         self.lookup_tables = TransformerWord2VecEncoder._create_lookup_tables(attribute_keys_tensor, extracted_w2v_models)
@@ -44,7 +49,7 @@ class TransformerWord2VecEncoder(Layer):
         return attribute_key_dict, attribute_keys_tensor
 
     @staticmethod
-    def _create_attribute_masks(attribute_keys, attribute_key_dict, sequence_length):
+    def _create_attribute_masks(attribute_keys, attribute_key_dict, case_length):
         # Convert attribute keys to integers based on the encoders
         attribute_keys_event_mask = []
         attribute_categorical_mask = []
@@ -60,9 +65,6 @@ class TransformerWord2VecEncoder(Layer):
         print("attribute_keys_event_mask", attribute_keys_event_mask)
 
         # Expand the attribute keys to the sequence length
-        case_length = sequence_length / len(attribute_keys)
-        case_length = tf.cast(case_length, tf.int32)
-        print("case_length", case_length)
         attribute_key_trace_mask = tf.tile(attribute_keys_event_mask, multiples=[case_length])
         categorical_mask = tf.tile(attribute_categorical_mask, multiples=[case_length])
         numerical_mask = tf.logical_not(categorical_mask)
@@ -70,7 +72,7 @@ class TransformerWord2VecEncoder(Layer):
         # print("attribute_key_trace_mask", self.attribute_key_trace_mask)
         # print("trace_categorical_mask", self.categorical_mask)
 
-        return attribute_key_trace_mask, categorical_mask, numerical_mask
+        return attribute_key_trace_mask, categorical_mask, numerical_mask, attribute_keys_event_mask
 
     @staticmethod
     def _convert_to_sentences(input):
@@ -139,47 +141,75 @@ class TransformerWord2VecEncoder(Layer):
         print("lookup_tables", lookup_tables.keys())
         return lookup_tables
 
-    def process_attribute(self, value, key):
-        if key == -1:  # Numerical thus skip
-            return tf.zeros([self.dim_model], dtype=tf.float32)
-        else:  # Categorical
-            lookup_table = self.lookup_tables[key.numpy()]
-            result = lookup_table.lookup(value)
-            return result
-        
-    def process_trace(self, trace):
-        # Split categorical and numerical attributes once, and expand them efficiently
-        numerical_values = tf.where(self.numerical_mask, trace, tf.zeros_like(trace))
-        categorical_values = tf.where(self.categorical_mask, trace, tf.zeros_like(trace))
-
-        # Numerical: 
-        # Expand and tile the values to match the model dimension
-        numerical_values_result = tf.tile(tf.expand_dims(numerical_values, axis=-1), [1, self.dim_model])
-
-        # Categorical:
-        # RCVDB: TODO: The stack+lookup is very slow, compared to just expanding the values
-        # Potentially this can be optimized by splitting all the categorical values too
-        # categorical_values_result = tf.tile(tf.expand_dims(categorical_values, axis=-1), [1, self.dim_model])
-
-        categorical_values_int = tf.cast(categorical_values, dtype=tf.int32)
-        # print("categorical_values_int", categorical_values_int.shape)
-        categorical_values_result = tf.stack([
-            self.process_attribute(value, self.attribute_key_trace_mask[i])
-            for i, value in enumerate(categorical_values_int)
-        ])
-
-        # Combine numerical and categorical results
-        return categorical_values_result + numerical_values_result
-        
-    def call(self, inputs):
-        # print("encoding_call", inputs.shape)
-        result = tf.map_fn(
-            self.process_trace,
-            inputs,
-            fn_output_signature=tf.TensorSpec((self.sequence_length, self.dim_model), dtype=tf.float32)
+    def reshape_to_attributes_first(self, input_tensor):
+        batch_size = tf.shape(input_tensor)[0]
+        reshaped = tf.reshape(
+            input_tensor, 
+            (batch_size, self.case_length, self.num_attributes)
         )
-        # print("encoding_call_result", result.shape)
+        transposed = tf.transpose(reshaped, perm=[2, 1, 0])  # `[num_attributes, case_length, batch_size]`
+        final_shape = tf.reshape(transposed, (self.num_attributes, self.case_length * batch_size))
+        return final_shape
+
+    def expand_to_dim_model(self, input_tensor):
+        unique_masks = tf.unique(self.attribute_mask).y  # Extract unique mask values
+        expanded_tensors = []  # To store expanded tensors for each group
+
+        for mask_value in unique_masks:
+            # Create a mask for the current attribute value
+            group_mask = self.attribute_mask == mask_value
+            group_tensor = tf.boolean_mask(input_tensor, group_mask, axis=0)
+
+            if mask_value == -1:
+                # Expand numerical values to `dim_model`
+                expanded_group_tensor = tf.expand_dims(group_tensor, axis=-1)
+                expanded_group_tensor = tf.tile(expanded_group_tensor, [1, 1, self.dim_model])
+                # print("expanded_group_tensor_numerical", expanded_group_tensor.shape)
+            else:
+                # Expand categorical values to `dim_model`
+                group_tensor = tf.cast(group_tensor, dtype=tf.int32)
+                group_tensor = tf.reshape(group_tensor, [-1]) # Flatten
+                # print("group_tensor", group_tensor.shape)
+                # print(group_tensor)
+                expanded_group_tensor = self.lookup_tables[mask_value.numpy()].lookup(group_tensor)
+                expanded_group_tensor = tf.expand_dims(expanded_group_tensor, axis=0)
+
+                # print("expanded_group_tensor_categorical", expanded_group_tensor.shape)
+            
+            expanded_tensors.append(expanded_group_tensor)
+
+        # Combine all groups back together
+        result = tf.concat(expanded_tensors, axis=0)
+        # print("expanded_tensors", result.shape)
+
         return result
+
+    def reshape_to_batch_first(self, input_tensor, batch_size):
+        reshaped = tf.reshape(
+            input_tensor, 
+            (self.num_attributes, self.case_length, batch_size, self.dim_model)
+        )
+        transposed = tf.transpose(reshaped, perm=[2, 1, 0, 3])  # `[batch_size, case_length, num_attributes, dim_model]`
+        final_shape = tf.reshape(
+            transposed, 
+            (batch_size, self.case_length * self.num_attributes, self.dim_model)
+        )
+        return final_shape
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+
+        # Step 1: Reshape to `[num_attributes, case_length * batch_size]`
+        attributes_first = self.reshape_to_attributes_first(inputs)
+
+        # Step 2: Expand to `[num_attributes, case_length * batch_size, dim_model]`
+        expanded_tensor = self.expand_to_dim_model(attributes_first)
+
+        # Step 3: Transform back to `[batch_size, case_length * num_attributes, dim_model]`
+        output_tensor = self.reshape_to_batch_first(expanded_tensor, batch_size)
+
+        return output_tensor
+
     
 class ProcessWord2VecEncoder():
     def __init__(self,
